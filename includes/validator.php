027fb3417dc44c56aadc06d455331cd939d1f323
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-13 16:42:50 +0100

    Update validator.php

--- includes/validator.php
+++ includes/validator.php
@@ -61,66 +61,95 @@ class Validator {
         }
     }
 
+    /* Validating the sender to make sure it is a valid WhatsApp number. */
     static function validateSender($sender) {
+        /* Validating the sender to make sure it is a valid WhatsApp number. */
         $sender = filter_var($sender, FILTER_VALIDATE_REGEXP, [
             'options' => [
                 'regexp' => '/^[0-9]{11,15}$/'
             ]
         ]);
+        /* Checking if the sender is a valid WhatsApp number. */
         if ($sender === false) {
+           /* An array that contains the status of the validation and the message. */
             $info = [
                 'status' => false,
                 'message' => 'Sender is not a valid WhatsApp Number'
             ];
             
+            /* Returning the array as a json object. */
             return json_encode($info);
         } else {
+           /* An array that contains the status of the validation and the message. */
             $info = [
                 'status' => true,
                 'message' => 'Valid sender'
             ];
+           /* Returning the array as a json object. */
             return json_encode($info);
         }
     }
 
+    /* Validating the token. */
     static function validateToken($token) {
+        /* Validating the token to make sure it is a valid token. */
         $token = filter_var($token, FILTER_VALIDATE_REGEXP, [
+            // A regular expression that checks if the token is a valid token.
             'options' => [
                 'regexp' => '/^[a-zA-Z0-9]{20}$/'
             ]
         ]);
+       /* Checking if the token is valid. */
         if ($token === false) {
+           /* An array that contains the status of the validation and the message. */
             $info = [
                 'status' => false,
                 'message' => 'Invalid Token. Visit Trenalyze to get a token'
             ];
             
+            /* Returning the array as a json object. */
             return json_encode($info);
-        } else {
+        } 
+        /* Returning a json object that contains the status of the validation and the message. */
+        else {
+            /* An array that contains the status of the validation and the message. */
             $info = [
                 'status' => true,
                 'message' => 'Valid token'
             ];
+
+            /* Returning the array as a json object. */
             return json_encode($info);
         }
     }
 
+
+    /* Validating the api url. */
     static function validateApiUrl($apiUrl) {
+       /* Validating the url to make sure it is a valid url. */
         $url = filter_var($apiUrl, FILTER_VALIDATE_URL);
+
+        /* Checking if the url is valid and if it is, it checks if the url is the default url. */
         if ($url) {
-            if ($apiUrl === 'https://trenalyze.com/public/api') {
+            /* Checking if the api url is the default url. */
+            if ($apiUrl === 'https://trenalyze.com/api') {
+                /* An array that contains the status of the validation and the message. */
                 $info = [
-                    'status' =>true,
+                    'status' => true,
                     'message' => 'Valid API URL'
                 ];
 
+                /* Returning the array as a json object. */
                 return json_encode($info);
             } else {
+
+                /* An array that contains the status of the validation and the message. */
                 $info = [
                     'status' => false,
-                    'message' => 'Invalid API URL. It must be https://trenalyze.com/public/api'
+                    'message' => 'Invalid API URL. It must be https://trenalyze.com/api'
                 ];
 
+                /* Returning the array as a json object. */
                 return json_encode($info);
             }
         } else {
@@ -128,6 +157,7 @@ class Validator {
                 'status' => false,
                 'message' => 'Invalid API URL. Please don\'t Change the Default'
             ];
+            /* Returning the array as a json object. */
             return json_encode($info);
         }
     }
